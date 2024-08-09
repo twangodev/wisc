78c29e962670b040d63ffdd9912755b8f5fd15cd
James Ding <jamesding365@gmail.com>
2024-08-09 12:10:09 +0300

    Update course_details.py

--- course_details.py
+++ course_details.py
@@ -1,14 +1,17 @@
 import json
 import re
+from datetime import datetime, timezone
 
 import requests
 from bs4 import BeautifulSoup
 
-scrape_url = 'https://guide.wisc.edu/courses/comp_sci/'
+cs_url = 'https://guide.wisc.edu/courses/comp_sci/'
+stat_url = 'https://guide.wisc.edu/courses/stat/'
+math_url = 'https://guide.wisc.edu/courses/math/'
 
 def extract_course_numbers(text):
     # Define the regex pattern to match course numbers (e.g., 102, 200, 240, etc.)
-    pattern = re.compile(r'\d{3}$')
+    pattern = re.compile(r'\d{2,3}$')
 
     # Find all matches in the provided text
     matches = pattern.search(text)
@@ -33,6 +36,7 @@ def get_course_details(url):
     for course_block in course_blocks:
 
         block_code = course_block.find('span', class_='courseblockcode').get_text(strip=True)
+        print(f'Discovered: {block_code}');
         number = extract_course_numbers(block_code)
 
         # Extract course title
@@ -52,9 +56,19 @@ def get_course_details(url):
 
 # Call the function to get course requirements
 if __name__ == '__main__':
-    data = get_course_details(scrape_url)
+    cs = get_course_details(cs_url)
+    math = get_course_details(math_url)
+    stat = get_course_details(stat_url)
+
+    data = {
+        'last_updated': datetime.now(timezone.utc).isoformat(),
+        'COMP SCI': cs,
+        'MATH': math,
+        'STAT': stat,
+    }
+
 
     # Write data to JSON file called data.json in docs/cs-course-map
     with open('docs/cs-course-map/data.json', 'w') as f:
-        json.dump(data, f)
+        json.dump(data, f, indent=4)
 
