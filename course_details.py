d3b91f5374c245825ae18c592729f8fe1b300508
James Ding <jamesding365@gmail.com>
2024-08-09 10:18:06 +0300

    Add course_details.py and result

---
+++ course_details.py
@@ -0,0 +1,60 @@
+import json
+import re
+
+import requests
+from bs4 import BeautifulSoup
+
+scrape_url = 'https://guide.wisc.edu/courses/comp_sci/'
+
+def extract_course_numbers(text):
+    # Define the regex pattern to match course numbers (e.g., 102, 200, 240, etc.)
+    pattern = re.compile(r'\d{3}$')
+
+    # Find all matches in the provided text
+    matches = pattern.search(text)
+
+    return matches.group(0)
+
+def get_course_details(url):
+    # Fetch the HTML content from the URL
+    response = requests.get(url)
+    response.raise_for_status()  # Check if the request was successful
+    html_content = response.text
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Find all course blocks
+    course_blocks = soup.find_all('div', class_='courseblock')
+
+    course_details = {}
+
+    # Iterate over each course block to extract course information
+    for course_block in course_blocks:
+
+        block_code = course_block.find('span', class_='courseblockcode').get_text(strip=True)
+        number = extract_course_numbers(block_code)
+
+        # Extract course title
+        title = course_block.find('p', class_='courseblocktitle').strong.get_text(strip=True)
+        title = title.split('—')[-1].strip()
+
+        # Extract course description
+        description = course_block.find('p', class_='courseblockdesc').get_text(strip=True)
+
+        # Store the extracted information in the dictionary
+        course_details[number] = {
+            'title': title,
+            'description': description,
+        }
+
+    return course_details
+
+# Call the function to get course requirements
+if __name__ == '__main__':
+    data = get_course_details(scrape_url)
+
+    # Write data to JSON file called data.json in docs/cs-course-map
+    with open('docs/cs-course-map/data.json', 'w') as f:
+        json.dump(data, f)
+
